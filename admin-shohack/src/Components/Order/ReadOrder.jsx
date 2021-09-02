641e299957c8e37793f46358ccebe77bf82f92da
rotza1802 <65125661+rotza1802@users.noreply.github.com>
2021-09-02 11:19:21 -0300

    Se creo el modal de para ver cada orden

--- admin-shohack/src/Components/Order/ReadOrder.jsx
+++ admin-shohack/src/Components/Order/ReadOrder.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import SidebarMenu from "../Dashboard/SidebarMenu";
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import axios from "axios";
+import ShowOrder from "./ShowOrder";
+import { Modal, Button } from "react-bootstrap";
 
 function ReadOrder() {
   const [orders, setOrders] = useState([]);
   const user = useSelector((state) => state.user);
+  const [Show, setShow] = useState(false);
+  const [clickedOrder, setClickedOrder] = useState({});
+  let orderList = clickedOrder.orderList;
+
+  const showOrderModal = (order) => {
+    setClickedOrder(order);
+    setShow(true);
+  };
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   useEffect(() => {
     const getOrder = async () => {
@@ -25,47 +37,88 @@ function ReadOrder() {
   }, []);
 
   return (
-    <div className="d-flex justify-content">
-      <div>
-        <SidebarMenu />
+    <>
+      <div className="d-flex justify-content">
+        <div>
+          <SidebarMenu />
+        </div>
+        <div className="mx-auto w-100 p-2">
+          <p className="text-center fw-bold fs-3">ORDENES</p>
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th>Numero de orden</th>
+                <th>Fecha</th>
+                <th>Usuario</th>
+                <th>Total</th>
+                <th>Estado</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              {orders.map((order) => {
+                return (
+                  <tr>
+                    <th>{order._id}</th>
+                    <th>20/07/21</th>
+                    <th>{order.user}</th>
+                    <th>$ {order.totalPrice}</th>
+                    <th>{order.state}</th>
+                    <th>
+                      <button
+                        className="btn btn-dark text-white"
+                        onClick={() => showOrderModal(order)}
+                      >
+                        Ver orden
+                      </button>
+                    </th>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-      <div className="mx-auto w-100 p-2">
-        <p className="text-center fw-bold fs-3">ORDENES</p>
-        <table class="table table-striped">
-          <thead>
-            <tr>
-              <th>Numero de orden</th>
-              <th>Fecha</th>
-              <th>Usuario</th>
-              <th>Total</th>
-              <th>Estado</th>
-              <th>Acción</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => {
-              return (
-                <tr>
-                  <th>{order._id}</th>
-                  <th>20/07/21</th>
-                  <th>{order.user}</th>
-                  <th>$ {order.totalPrice}</th>
-                  <th>{order.state}</th>
-                  <th>
-                    <Link
-                      /* to={`/orden/${order._id}`} */
-                      className="btn btn-dark text-white me-2"
-                    >
-                      Ver orden
-                    </Link>
-                  </th>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
-    </div>
+      {/* <ShowOrder
+        Show={Show}
+        setShow={setShow}
+        handleClose={handleClose}
+        handleShow={handleShow}
+        clickedOrder={clickedOrder}
+      /> */}
+      <Modal
+        show={Show}
+        onHide={handleClose}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            ORDEN N: {clickedOrder._id}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h4>{clickedOrder.user} - 20/07/21 </h4>
+          <hr />
+          {orderList.map((order) => {
+            return (
+              <div className="py-2">
+                <p className="fw-bold">{order.name}</p>
+                <p>$ {order.price}</p>
+                <p>x {order.quantity}</p>
+              </div>
+            );
+          })}
+          <hr />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button className="btn btn-dark text-white" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
